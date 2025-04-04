bcfce15745b7f23448c3c2c7f912f32de74da4ec
JhonatanCremona <jfulguera@creminox.com>
2025-04-04 14:55:49 -0300

    actualizar docker deploy

--- query/insert_recetario.sql
+++ query/insert_recetario.sql
@@ -1,10 +1,10 @@
-INSERT INTO recetario (id, codigoProducto, nroGripper, tipoMolde, anchoProducto, altoProducto, largoProducto, pesoProducto, moldesNivel, altoMolde, largoMolde, ajusteAltura, cantidadNiveles, deltaNiveles, n1Altura, bastidorAltura, ajusteN1Altura) VALUES
-(1, 'Ovalado', 3, 'Tipo A', 87, 84, 104, 178, 2, 147, 92, 19, 11, 13, 97, 180, 17),
-(2, 'Cuadrado', 3, 'Tipo A', 76, 57, 110, 429, 1, 128, 150, 13, 11, 6, 84, 106, 6),
-(3, 'Rectangular 7K', 3, 'Tipo B', 90, 189, 186, 266, 2, 135, 125, 15, 10, 5, 76, 111, 14),
-(4, 'Mandolina 6K', 1, 'Tipo B', 179, 196, 61, 276, 4, 89, 131, 14, 11, 5, 119, 168, 18),
-(5, 'Lunch', 2, 'Tipo A', 65, 140, 144, 211, 3, 102, 52, 20, 10, 9, 73, 178, 19),
-(6, 'Cuadrado Largo', 1, 'Tipo A', 60, 92, 101, 301, 5, 142, 136, 13, 11, 11, 85, 174, 7),
-(7, 'Redondo', 1, 'Tipo C', 81, 87, 175, 361, 4, 91, 107, 10, 9, 14, 102, 149, 21),
-(8, 'Ahumado', 3, 'Tipo B', 123, 56, 154, 232, 1, 69, 81, 7, 8, 7, 113, 139, 17),
-(9, 'Queso Puerco', 2, 'Tipo C', 118, 133, 183, 224, 1, 135, 70, 10, 11, 13, 118, 124, 20);
+INSERT INTO recetario (id, codigoProducto, nroGripper, tipoMolde, anchoProducto, altoProducto, largoProducto, pesoProducto, moldesNivel, altoMolde, largoMolde, ajusteAltura, cantidadNiveles, deltaNiveles, n1Altura, bastidorAltura, ajusteN1Altura, productosMolde) VALUES
+(1, 'Ovalado', 2, 'Molde B', 68, 103, 135, 138, 5, 123, 127, 8, 10, 5, 77, 170, 9, 1),
+(2, 'Cuadrado', 1, 'Molde A', 103, 152, 80, 385, 2, 52, 95, 17, 8, 9, 74, 165, 20, 4),
+(3, 'Rectangular 7K', 3, 'Molde A', 149, 109, 172, 101, 4, 80, 127, 7, 8, 5, 86, 136, 17, 2),
+(4, 'Mandolina 6K', 1, 'Molde A', 184, 151, 173, 403, 5, 131, 79, 18, 8, 13, 63, 159, 5, 4),
+(5, 'Lunch', 1, 'Molde B', 62, 139, 195, 170, 2, 50, 71, 5, 8, 6, 57, 153, 13, 1),
+(6, 'Cuadrado Largo', 2, 'Molde C', 137, 118, 66, 396, 3, 83, 74, 16, 9, 13, 60, 140, 6, 1),
+(7, 'Redondo', 2, 'Molde B', 144, 141, 98, 481, 3, 76, 135, 19, 8, 9, 89, 151, 24, 1),
+(8, 'Ahumado', 3, 'Molde A', 162, 114, 118, 128, 3, 92, 63, 19, 9, 10, 85, 157, 21, 4),
+(9, 'Queso Puerco', 2, 'Molde C', 170, 137, 65, 492, 3, 98, 98, 18, 10, 14, 84, 148, 17, 5);
