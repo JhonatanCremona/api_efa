e45aa8f84124351b584e3afd91accb08c1c59c01
JhonatanCremona <jfulguera@creminox.com>
2025-04-16 12:47:35 -0300

    actualizacion de diccionario de base de datos

--- query/insert_recetario.sql
+++ query/insert_recetario.sql
@@ -1,10 +1,9 @@
 INSERT INTO recetario (id, codigoProducto, nroGripper, tipoMolde, anchoProducto, altoProducto, largoProducto, pesoProducto, moldesNivel, altoMolde, largoMolde, ajusteAltura, cantidadNiveles, deltaNiveles, n1Altura, bastidorAltura, ajusteN1Altura, productosMolde) VALUES
-(1, 'Ovalado', 2, 'Molde B', 68, 103, 135, 138, 5, 123, 127, 8, 10, 5, 77, 170, 9, 1),
-(2, 'Cuadrado', 1, 'Molde A', 103, 152, 80, 385, 2, 52, 95, 17, 8, 9, 74, 165, 20, 4),
-(3, 'Rectangular 7K', 3, 'Molde A', 149, 109, 172, 101, 4, 80, 127, 7, 8, 5, 86, 136, 17, 2),
-(4, 'Mandolina 6K', 1, 'Molde A', 184, 151, 173, 403, 5, 131, 79, 18, 8, 13, 63, 159, 5, 4),
-(5, 'Lunch', 1, 'Molde B', 62, 139, 195, 170, 2, 50, 71, 5, 8, 6, 57, 153, 13, 1),
-(6, 'Cuadrado Largo', 2, 'Molde C', 137, 118, 66, 396, 3, 83, 74, 16, 9, 13, 60, 140, 6, 1),
-(7, 'Redondo', 2, 'Molde B', 144, 141, 98, 481, 3, 76, 135, 19, 8, 9, 89, 151, 24, 1),
-(8, 'Ahumado', 3, 'Molde A', 162, 114, 118, 128, 3, 92, 63, 19, 9, 10, 85, 157, 21, 4),
-(9, 'Queso Puerco', 2, 'Molde C', 170, 137, 65, 492, 3, 98, 98, 18, 10, 14, 84, 148, 17, 5);
+(1, 'Cuadrado', 0, '', 0, 0, 0, 0, 0, 0, 0, 0, 11, 0, 0, 0, 0, 0),
+(2, 'Rectangular 7K', 0, '', 0, 0, 0, 0, 0, 0, 0, 0, 11, 0, 0, 0, 0, 0),
+(3, 'Mandolina 6K', 0, '', 0, 0, 0, 0, 0, 0, 0, 0, 11, 0, 0, 0, 0, 0),
+(4, 'Ovalado A', 0, '', 0, 0, 0, 0, 0, 0, 0, 0, 11, 0, 0, 0, 0, 0),
+(5, 'Ovalado B', 0, '', 0, 0, 0, 0, 0, 0, 0, 0, 11, 0, 0, 0, 0, 0),
+(6, 'Lunch', 0, '', 0, 0, 0, 0, 0, 0, 0, 0, 11, 0, 0, 0, 0, 0),
+(7, 'Cuadrado Largo', 0, '', 0, 0, 0, 0, 0, 0, 0, 0, 11, 0, 0, 0, 0, 0),
+(8, 'Queso Puerco', 0, '', 0, 0, 0, 0, 0, 0, 0, 0, 11, 0, 0, 0, 0, 0);
